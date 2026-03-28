409f6199d038dd70c643e10bab9abb90c5744420
elhakim <wowon48@gmail.com>
2026-03-28 14:50:29 +0700

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,22 +1,22 @@
 function downloadPDF(){
 
-const { jsPDF } = window.jspdf;
+  if(!window.jspdf){
+    alert("Library jsPDF belum dimuat!");
+    return;
+  }
 
-let doc = new jsPDF();
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF();
 
-doc.text("TABUNGAN UMROH",20,20);
+  let nama = document.getElementById("nama").innerText;
+  let saldo = document.getElementById("saldo").innerText;
 
-doc.text(
-"Nama : "+document.getElementById("nama").innerText,
-20,40
-);
+  doc.setFontSize(14);
+  doc.text("TABUNGAN UMROH ELHAKIM", 20, 20);
 
-doc.text(
-"Total Tabungan : "+
-document.getElementById("saldo").innerText,
-20,50
-);
+  doc.setFontSize(11);
+  doc.text("Nama : " + nama, 20, 40);
+  doc.text("Total Tabungan : " + saldo, 20, 50);
 
-doc.save("tabungan_umroh.pdf");
-
+  doc.save("tabungan_umroh.pdf");
 }
