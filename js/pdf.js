eea194823c8206e7bfba98331be56d52b7e6d500
elhakim <wowon48@gmail.com>
2026-04-03 15:21:11 +0700

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -3,24 +3,35 @@ function downloadPDF() {
 const { jsPDF } = window.jspdf;
 const doc = new jsPDF();
 
-// ambil data
+// data
 let nama = document.getElementById("nama").innerText;
 let saldo = document.getElementById("saldo").innerText;
 
-// load logo
-let img = new Image();
-img.src = "img/logo.png";
+// ======================
+// LOAD IMAGE DENGAN FETCH
+// ======================
+fetch("img/logo.png")
+.then(res => res.blob())
+.then(blob => {
 
-img.onload = function(){
+    let reader = new FileReader();
 
-doc.addImage(img, "PNG", 15, 10, 30, 15);
+    reader.onloadend = function() {
 
-doc.text("LAPORAN TABUNGAN UMROH", 20, 40);
-doc.text("Nama: " + nama, 20, 50);
-doc.text("Saldo: " + saldo, 20, 60);
+        let base64data = reader.result;
 
-doc.save("laporan.pdf");
+        // baru masuk ke PDF
+        doc.addImage(base64data, "PNG", 15, 10, 30, 15);
 
-};
+        doc.text("LAPORAN TABUNGAN UMROH", 20, 40);
+        doc.text("Nama: " + nama, 20, 50);
+        doc.text("Saldo: " + saldo, 20, 60);
+
+        doc.save("laporan.pdf");
+    };
+
+    reader.readAsDataURL(blob);
+
+});
 
 }
