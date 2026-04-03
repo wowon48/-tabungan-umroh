b2812b077a343a0b7190bd4e85958c14337744a5
elhakim <wowon48@gmail.com>
2026-04-03 15:11:14 +0700

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -7,12 +7,20 @@ const doc = new jsPDF();
 let nama = document.getElementById("nama").innerText;
 let saldo = document.getElementById("saldo").innerText;
 
-// test basic
-doc.text("LAPORAN TABUNGAN UMROH", 20, 20);
-doc.text("Nama: " + nama, 20, 30);
-doc.text("Saldo: " + saldo, 20, 40);
+// load logo
+let img = new Image();
+img.src = "img/logo.png";
 
-// SAVE
-doc.save("test.pdf");
+img.onload = function(){
+
+doc.addImage(img, "PNG", 15, 10, 30, 15);
+
+doc.text("LAPORAN TABUNGAN UMROH", 20, 40);
+doc.text("Nama: " + nama, 20, 50);
+doc.text("Saldo: " + saldo, 20, 60);
+
+doc.save("laporan.pdf");
+
+};
 
 }
